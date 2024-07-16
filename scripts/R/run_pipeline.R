38c84beb49b37ddb803d48b0039164bbdc927dcf
Michael Aydinbas <michael.aydinbas@gmail.com>
2024-07-16 19:33:46 +0200

    add step 4 to pipeline: processing and uploading logs to database

--- scripts/R/run_pipeline.R
+++ scripts/R/run_pipeline.R
@@ -78,6 +78,7 @@ download_data(bucket = BUCKET_DOWLOAD, data_dir = data_dir)
 source("scripts/R/run_script_1_extract_raw_data.R") # creates CSV files in subfolders patient_data_raw and product_data_raw
 source("scripts/R/run_script_2_clean_data.R") # creates CSV files in subfolders patient_data_cleaned and product_data_cleaned
 source("scripts/R/run_script_3_create_tables.R") # creates final CSV files in subfolder tables
+source("scripts/R/run_script_4_ingest_logs.R")
 upload_data(bucket = BUCKET_UPLOAD, data_dir = output_dir)
 ingest_data(
     project_id = PROJECT_ID,
@@ -113,4 +114,11 @@ ingest_data(
     dataset = DATASET,
     table = "clinic_data_static",
     source = file.path(table_dir, "clinic_data_static.parquet")
+)
+ingest_data(
+    project_id = PROJECT_ID,
+    cluster_fields = "script,functionName,errorCode,warningCode,fileName",
+    dataset = DATASET,
+    table = "logs",
+    source = file.path(table_dir, "table_logs.parquet")
 )
